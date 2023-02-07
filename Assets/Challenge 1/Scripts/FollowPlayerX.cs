9ed83a52a054602c35536fe345d676855fc73541
Hash2O <52819468+Hash2O@users.noreply.github.com>
2023-02-07 18:55:04 +0100

    Updates

--- Assets/Challenge 1/Scripts/FollowPlayerX.cs
+++ Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -7,14 +7,8 @@ public class FollowPlayerX : MonoBehaviour
     public GameObject plane;
     private Vector3 offset = new Vector3(26, 3.75f, 0.32f);
 
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
         transform.position = plane.transform.position + offset;
     }
--- Assets/Challenge 1/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -4,35 +4,53 @@ using UnityEngine;
 
 public class PlayerControllerX : MonoBehaviour
 {
-    public float speed = 10.0f;
-    public float rotationSpeed = 25.0f;
+    public float speed = 20.0f;
+    public float rotationSpeed = 50.0f;
     public float verticalInput;
     public float horizontalInput;
-
+    public Camera camOnBoard;
+    public bool isCamOnBoardActive;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        camOnBoard = GameObject.Find("Camera").GetComponent<Camera>();
+        isCamOnBoardActive = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        // get the user's vertical input
-        verticalInput = Input.GetAxis("Horizontal");
-        horizontalInput = Input.GetAxis("Vertical");
+        if(Input.GetKeyDown(KeyCode.C))
+        {
+            if(isCamOnBoardActive == false)
+            {
+                isCamOnBoardActive = true;
+                camOnBoard.gameObject.SetActive(true);
+            }
+            else
+            {
+                isCamOnBoardActive = false;
+                camOnBoard.gameObject.SetActive(false);
+            }
+            
+        }
+        
 
-        // move the plane forward at a constant rate
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        // get the user's vertical input
+        verticalInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        // move the car forward 
+        transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);
 
         // turn left or right
-        transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime * horizontalInput);
+        transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime * horizontalInput);
 
         //transform.Rotate(0, 0, rotationSpeed * Time.deltaTime * horizontalInput);
 
         // tilt the plane up/down based on up/down arrow keys
-        transform.Rotate(Vector3.up, rotationSpeed * verticalInput * Time.deltaTime);
+        //transform.Rotate(Vector3.up, rotationSpeed * verticalInput * Time.deltaTime);
 
     }
 }
